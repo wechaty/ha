31ddf09a55a86b61eca0b3c39f7b20ffef2d20bb
Huan LI (李卓桓) <zixia@zixia.net>
2020-05-11 20:22:49 +0800

    move epic operations to operations.ts

--- src/wechaty-redux/ducks/operations.ts
+++ src/wechaty-redux/ducks/operations.ts
@@ -4,7 +4,63 @@
  *
  * Huan https://github.com/huan May 2020
  */
+import {
+  of,
+}                 from 'rxjs'
+import {
+  ignoreElements,
+  catchError,
+  mapTo,
+}                 from 'rxjs/operators'
+
+import {
+  getWechaty,
+}                 from '../wechaty-redux'
+
+import * as actions from './actions'
+
+const ding$ = (action: ReturnType<typeof actions.ding>) => of(
+  getWechaty(action.payload.wechatyId)
+    .ding(action.payload.data)
+).pipe(
+  ignoreElements(),
+  catchError(e => of(
+    actions.errorEvent(
+      action.payload.wechatyId,
+      { data: String(e) },
+    )
+  )),
+)
+
+const reset$ = (action: ReturnType<typeof actions.reset>) => of(
+  getWechaty(action.payload.wechatyId)
+    .reset(action.payload.data)
+).pipe(
+  ignoreElements(),
+  catchError(e => of(
+    actions.errorEvent(
+      action.payload.wechatyId,
+      { data: String(e) },
+    )
+  )),
+)
+
+const say$ = (action: ReturnType<typeof actions.sayAsync.request>) => of(
+  getWechaty(action.payload.wechatyId)
+    .say(action.payload.text)
+).pipe(
+  mapTo(actions.sayAsync.success(action.payload.wechatyId, action.payload.id)),
+  catchError(e => of(
+    actions.sayAsync.failure(
+      action.payload.wechatyId,
+      action.payload.id,
+      e,
+    )
+  ))
+)
 
 export {
-
+  ding$,
+  reset$,
+  say$,
 }
