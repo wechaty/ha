063cc9d5e7fb4cd3e2576645ff1a88aab0a447f2
Huan LI (李卓桓) <zixia@zixia.net>
2020-05-31 12:27:46 +0800

    Workaround for action typing (https://github.com/huan/ducks/issues/2)

--- src/wechaty-ducks-counter/actions.ts
+++ src/wechaty-ducks-counter/actions.ts
@@ -6,8 +6,10 @@ const prepareWechaty = (wechatyId: string) => ({ wechatyId })
 
 const moMessage = createAction(types.MESSAGE_MO, prepareWechaty)()
 const mtMessage = createAction(types.MESSAGE_MT, prepareWechaty)()
+const noop = createAction(types.NOOP)()
 
 export {
   moMessage,
   mtMessage,
+  noop,
 }
--- src/wechaty-ducks-counter/types.ts
+++ src/wechaty-ducks-counter/types.ts
@@ -1,7 +1,11 @@
 const MESSAGE_MO = 'counter/MESSAGE_MO' // Bot send message
 const MESSAGE_MT = 'counter/MESSAGE_MT' // Bot recv message
 
+// Workaround for https://github.com/huan/ducks/issues/2
+const NOOP = 'counter/NOOP'
+
 export {
   MESSAGE_MO,
   MESSAGE_MT,
+  NOOP,
 }
